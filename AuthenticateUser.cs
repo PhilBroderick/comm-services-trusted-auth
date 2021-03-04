ccbdf37b7d332cb607e2fcb8acbf2571b6a0d0bc
PhilBroderick <philbroderick12@gmail.com>
2021-03-04 15:56:13 +0000

    initial solution commit

---
+++ AuthenticateUser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Communication;
+using Azure.Communication.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CommServices.TrustedAuth
+{
+    public static class AuthenticateUser
+    {
+        private static readonly List<User> AuthenticatedUsers = new List<User>();
+        
+        [FunctionName("AuthenticateUser")]
+        public static async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            var userId = req.Query["userId"];
+            User user;
+
+            var connectionString = Environment.GetEnvironmentVariable("COMM_SERVICES_CONNECTION_STRING");
+            var client = new CommunicationIdentityClient(connectionString);
+            
+            if (string.IsNullOrWhiteSpace(userId) || AuthenticatedUsers.All(a => a.Id != userId))
+            {
+                var identityWithTokenResponse =
+                    await client.CreateUserWithTokenAsync(new[] {CommunicationTokenScope.Chat});
+
+                user = new User(identityWithTokenResponse.Value.user.Id,
+                    identityWithTokenResponse.Value.token.ExpiresOn, identityWithTokenResponse.Value.token.Token);
+                AuthenticatedUsers.Add(user);
+            }
+            else
+            {
+                var existingUser = AuthenticatedUsers.Single(a => a.Id == userId);
+                if (existingUser.ExpiresOn > DateTimeOffset.Now)
+                {
+                    user = existingUser;
+                }
+                else
+                {
+                    var identityToRefresh = new CommunicationUserIdentifier(existingUser.Id);
+                    var updatedToken =
+                        await client.IssueTokenAsync(identityToRefresh, new[] {CommunicationTokenScope.Chat });
+                    existingUser.AccessToken = updatedToken.Value.Token;
+                    existingUser.ExpiresOn = updatedToken.Value.ExpiresOn;
+                    user = existingUser;
+                }
+            }
+
+            return new OkObjectResult(user);
+        }
+    }
+
+    public class User
+    {
+        public User(string id, DateTimeOffset expiresOn, string accessToken)
+        {
+            Id = id;
+            ExpiresOn = expiresOn;
+            AccessToken = accessToken;
+        }
+        public string Id { get; }
+        public DateTimeOffset ExpiresOn { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+﻿# Introduction
+
+This is a very simple Azure function which acts as a trusted authentication service for 
+[Azure Communication Services](https://azure.microsoft.com/en-gb/services/communication-services/), with the `Chat` scope.
+
+This would be used for something like a basic anonymous chat app. It can be extended to only
+provide tokens to authenticated users. Currently users are stored in an in memory list for ease of access,
+but ideally this would be stored in a more persistent datastore.
+
+## Running Locally
+
+Clone the repository
+```shell
+git clone https://github.com/PhilBroderick/comm-services-trusted-auth
+```
+
+Create an Azure Communication Services resource
+```shell
+az communication create --name "<communicationName>" --location "Global" --data-location "United States" --resource-group "<resourceGroup>"
+```
+
+Retrieve the connection string
+```shell
+az communication list-key --name "<communicationName>" --resource-group "<resourceGroup>"
+```
+
+Add it is an environment variable called `COMM_SERVICES_CONNECTION_STRING`
+
+The solution can then be run through an IDE like Visual Studio or Visual Studio Code.
+
+Making a request to `http://localhost:7071/api/AuthenticateUser` will return a User object
+with an Id, Access Token and Expiry timestamp. This same Id can be used to generate a new token.
+
+This token can then be used when setting up a chat thread with Azure Communication Services.
+
+
